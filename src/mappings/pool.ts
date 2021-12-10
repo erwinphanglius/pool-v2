3cdb6998a7e832f6c09a889e882477e520557ad8
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-11 02:49:22 +0700

    mapping update for pool

---
+++ src/mappings/pool.ts
@@ -0,0 +1,52 @@
+import {
+    PoolCreation
+  } from "../../generated/PoolFactory/PoolFactory"
+import {
+  FundPool
+} from "../../generated/templates/MetaversepadTemplate/Metaversepad"
+import { Factory, Pool } from "../../generated/schema"
+
+export function handleFundPool(evtPoolCreation: PoolCreation, evtPoolInfo: FundPool): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
+  let entity = Pool.load(evtPoolCreation.params.poolAddress.toHexString())
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!entity) {
+    entity = new Pool(evtPoolCreation.params.poolAddress.toHexString())
+
+    // Entity fields can be set using simple assignments
+    // entity.count = BigInt.fromI32(0)
+  }
+
+  // BigInt and BigDecimal math are supported
+  // entity.count = entity.count + BigInt.fromI32(1)
+
+  // Entity fields can be set based on event parameters
+  entity.address = evtPoolInfo.params.initiator
+  entity.balance = evtPoolInfo.params.value
+
+  // Entities can be written to the store with `.save()`
+  entity.save()
+
+  // Note: If a handler doesn't require existing field values, it is faster
+  // _not_ to load the entity from the store. Instead, create it fresh with
+  // `new Entity(...)`, set the fields that should be updated and save the
+  // entity back to the store. Fields that were not set or unset remain
+  // unchanged, allowing for partial updates to be applied.
+
+  // It is also possible to access smart contracts from mappings. For
+  // example, the contract that has emitted the event can be connected to
+  // with:
+  //
+  // let contract = Contract.bind(event.address)
+  //
+  // The following functions can then be called on this contract to access
+  // state variables and other data:
+  //
+  // - contract.name(...)
+  // - contract.owner(...)
+}
+
+// export function handlePoolCreation(event: PoolCreation): void {}
