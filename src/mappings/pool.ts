8c2c3e224edf3fe7e11d36163d9d7952bba7fe8e
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-12 23:39:05 +0700

    redefine

--- src/mappings/pool.ts
+++ src/mappings/pool.ts
@@ -21,33 +21,31 @@ import { Factory, User, PoolByUser, Pool, PoolWithUser } from "../../generated/s
 
 export function handleFundPool(evtPoolInfo: FundPool): void {
   let entity = PoolByUser.load(evtPoolInfo.transaction.hash.toHex())
-  // let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
-  // let poolEntity = Pool.load(evtPoolInfo.address.toHex())
-  // let poolWithUserEntity = PoolWithUser.load(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator.toHex())
+  let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
+  let poolEntity = Pool.load(evtPoolInfo.address.toHex())
+  let poolWithUserEntity = PoolWithUser.load(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator.toHex())
   
   if (!entity) {
     entity = new PoolByUser(evtPoolInfo.transaction.hash.toHex())
   }
-  // if (!userEntity) {
-  //   userEntity = new User(evtPoolInfo.params.initiator.toHex())
-  // }
-  // if (!poolEntity) {
-  //   poolEntity = new Pool(evtPoolInfo.address.toHex())
-  // }
-  // if (!poolWithUserEntity) {
-  //   poolWithUserEntity = new PoolWithUser(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator.toHex())
-  // }
+  if (!userEntity) {
+    userEntity = new User(evtPoolInfo.params.initiator.toHex())
+  }
+  if (!poolEntity) {
+    poolEntity = new Pool(evtPoolInfo.address.toHex())
+  }
+  if (!poolWithUserEntity) {
+    poolWithUserEntity = new PoolWithUser(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator.toHex())
+  }
   
   entity.poolAddress = evtPoolInfo.address;
   entity.user = evtPoolInfo.params.initiator;
   entity.value = evtPoolInfo.params.value;
   
-  // userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
-  // userEntity.pool = [poolEntity.id]
+  userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
+  userEntity.pool = [poolEntity.id]
 
-  // poolWithUserEntity.poolAddress = evtPoolInfo.address;
-  // poolWithUserEntity.user = evtPoolInfo.params.initiator;
-  // poolWithUserEntity.value = evtPoolInfo.params.value;
+  poolWithUserEntity.value = poolWithUserEntity.value.plus(evtPoolInfo.params.value);
   // poolEntity.members = [userEntity.id]
 
   // let id = address.toHexString();
@@ -69,9 +67,9 @@ export function handleFundPool(evtPoolInfo: FundPool): void {
 
   // participantEntity.save()  
   entity.save()
-  // userEntity.save()
-  // poolEntity.save()
-  // poolWithUserEntity.save()
+  userEntity.save()
+  poolEntity.save()
+  poolWithUserEntity.save()
 
 }
 
