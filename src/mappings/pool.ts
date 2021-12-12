5e7cff67dd618059e2b3c891338152c68045ad62
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-13 01:39:16 +0700

    ajdhk

--- src/mappings/pool.ts
+++ src/mappings/pool.ts
@@ -26,32 +26,32 @@ import { Factory, User, PoolByUser, Pool, PoolWithUser } from "../../generated/s
 
 export function handleFundPool(evtPoolInfo: FundPool): void {
   let entity = PoolByUser.load(evtPoolInfo.transaction.hash.toHex())
-  // let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
-  // let poolEntity = Pool.load(evtPoolInfo.address.toHex())
-  // let poolWithUserEntity = PoolWithUser.load(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator.toHex())
+  let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
+  let poolEntity = Pool.load(evtPoolInfo.address.toHex())
+  let poolWithUserEntity = PoolWithUser.load(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator.toHex())
   
   if (!entity) {
     entity = new PoolByUser(evtPoolInfo.transaction.hash.toHex())
   }
-  // if (!userEntity) {
-  //   userEntity = new User(evtPoolInfo.params.initiator.toHex())
-  // }
-  // if (!poolEntity) {
-  //   poolEntity = new Pool(evtPoolInfo.address.toHex())
-  // }
-  // if (!poolWithUserEntity) {
-  //   poolWithUserEntity = new PoolWithUser(evtPoolInfo.params.initiator.toHex() + "-" + evtPoolInfo.address.toHex())
-  // }
+  if (!userEntity) {
+    userEntity = new User(evtPoolInfo.params.initiator.toHex())
+  }
+  if (!poolEntity) {
+    poolEntity = new Pool(evtPoolInfo.address.toHex())
+  }
+  if (!poolWithUserEntity) {
+    poolWithUserEntity = new PoolWithUser(evtPoolInfo.params.initiator.toHex() + "-" + evtPoolInfo.address.toHex())
+  }
 
-  // entity.poolAddress = evtPoolInfo.address;
-  // entity.user = evtPoolInfo.params.initiator;
-  // entity.value = evtPoolInfo.params.value;
+  entity.poolAddress = evtPoolInfo.address;
+  entity.user = evtPoolInfo.params.initiator;
+  entity.value = evtPoolInfo.params.value;
   
-  // userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
-  // // userEntity.pool = [
+  userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
+  // userEntity.pool = [
 
-  // poolWithUserEntity.value = poolWithUserEntity.value.plus(evtPoolInfo.params.value);
-  // userEntity.pool.push(evtPoolInfo.address.toHex())
+  poolWithUserEntity.value = poolWithUserEntity.value.plus(evtPoolInfo.params.value);
+  userEntity.pool.push(evtPoolInfo.address.toHex())
   // poolEntity.user = evtPoolInfo.params.initiator;
   // poolEntity.value = evtPoolInfo.params.value.plus(evtPoolInfo.params.value)
 
@@ -75,9 +75,9 @@ export function handleFundPool(evtPoolInfo: FundPool): void {
 
   // participantEntity.save()  
   entity.save()
-  // userEntity.save()
-  // poolEntity.save()
-  // poolWithUserEntity.save()
+  userEntity.save()
+  poolEntity.save()
+  poolWithUserEntity.save()
 
 }
 
