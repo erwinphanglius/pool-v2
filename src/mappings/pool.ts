26d4d9088bab7a923c9a5205baf59dbeb8599382
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-12 23:26:46 +0700

    define new entity

--- src/mappings/pool.ts
+++ src/mappings/pool.ts
@@ -5,7 +5,7 @@ import {
 import {
   FundPool
 } from "../../generated/templates/MetaversepadTemplate/Metaversepad"
-import { Factory, User, PoolByUser, Pool } from "../../generated/schema"
+import { Factory, User, PoolByUser, Pool, PoolWithUser } from "../../generated/schema"
 
 // export function bindToUser(address: Address, poolAddress: Address, newBalance: BigInt): void {
 //   let id = address.toHexString();
@@ -23,6 +23,7 @@ export function handleFundPool(evtPoolInfo: FundPool): void {
   let entity = PoolByUser.load(evtPoolInfo.transaction.hash.toHex())
   let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
   let poolEntity = Pool.load(evtPoolInfo.address.toHex())
+  let poolWithUserEntity = PoolWithUser.load(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator)
   
   if (!entity) {
     entity = new PoolByUser(evtPoolInfo.transaction.hash.toHex())
@@ -33,6 +34,9 @@ export function handleFundPool(evtPoolInfo: FundPool): void {
   if (!poolEntity) {
     poolEntity = new Pool(evtPoolInfo.address.toHex())
   }
+  if (!poolWithUserEntity) {
+    poolWithUserEntity = new PoolWithUser(evtPoolInfo.address.toHex() + "-" + evtPoolInfo.params.initiator)
+  }
   
   entity.poolAddress = evtPoolInfo.address;
   entity.user = evtPoolInfo.params.initiator;
@@ -40,6 +44,10 @@ export function handleFundPool(evtPoolInfo: FundPool): void {
   
   userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
   userEntity.pool = [poolEntity.id]
+
+  poolWithUserEntity.poolAddress = evtPoolInfo.address;
+  poolWithUserEntity.user = evtPoolInfo.params.initiator;
+  poolWithUserEntity.value = evtPoolInfo.params.value;
   // poolEntity.members = [userEntity.id]
 
   // let id = address.toHexString();
