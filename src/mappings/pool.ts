c86b5d7ec7dc3e5f8fe7f8024dd83714a3b6a87d
erwinphanglius <erwinphanglius10@gmail.com>
2021-12-13 17:05:48 +0700

    userInPool schema

--- src/mappings/pool.ts
+++ src/mappings/pool.ts
@@ -5,34 +5,34 @@ import {
 import {
   FundPool
 } from "../../generated/templates/MetaversepadTemplate/Metaversepad"
-import { Factory,  Pool } from "../../generated/schema"
+import { Factory, User, Pool, UserInPool } from "../../generated/schema"
 
 export function handleFundPool(evtPoolInfo: FundPool): void {
-  // let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
+  let userEntity = User.load(evtPoolInfo.params.initiator.toHex())
   let poolEntity = Pool.load(evtPoolInfo.address.toHex())
-  // let userInPoolEntity = UserInPool.load(evtPoolInfo.params.initiator.toHex() + "-" + evtPoolInfo.address.toHex())
+  let userInPoolEntity = UserInPool.load(evtPoolInfo.params.initiator.toHex() + "-" + evtPoolInfo.address.toHex())
   
-  // if (!userEntity) {
-  //   userEntity = new User(evtPoolInfo.params.initiator.toHex())
-  // }
+  if (!userEntity) {
+    userEntity = new User(evtPoolInfo.params.initiator.toHex())
+  }
   if (!poolEntity) {
     poolEntity = new Pool(evtPoolInfo.address.toHex())
   }
-  // if (!userInPoolEntity) {
-  //   userInPoolEntity = new UserInPool(evtPoolInfo.params.initiator.toHex() + "-" + evtPoolInfo.address.toHex())
-  // }
+  if (!userInPoolEntity) {
+    userInPoolEntity = new UserInPool(evtPoolInfo.params.initiator.toHex() + "-" + evtPoolInfo.address.toHex())
+  }
   
-  // userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
+  userEntity.totalFundAllPool = userEntity.totalFundAllPool.plus(evtPoolInfo.params.value)
 
-  // let userPool = userEntity.pool;
-  // userPool.push(poolEntity.id)
-  // userEntity.pool = userPool;
-  // userEntity.save()
+  let userPool = userEntity.pool;
+  userPool.push(poolEntity.id)
+  userEntity.pool = userPool;
+  userEntity.save()
   
-  // userInPoolEntity.pool = evtPoolInfo.address;
-  // userInPoolEntity.user = evtPoolInfo.params.initiator;
-  // userInPoolEntity.value = userInPoolEntity.value.plus(evtPoolInfo.params.value);
+  userInPoolEntity.pool = evtPoolInfo.address;
+  userInPoolEntity.user = evtPoolInfo.params.initiator;
+  userInPoolEntity.value = userInPoolEntity.value.plus(evtPoolInfo.params.value);
   
   poolEntity.save()
-  // userInPoolEntity.save() 
+  userInPoolEntity.save() 
 }
